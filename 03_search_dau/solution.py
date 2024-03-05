413c93fc3e9578c102938b308ad8a7b429793763
Googlemogle <89452865+Googlemogle@users.noreply.github.com>
2024-03-05 12:20:04 +0300

    Add files via upload

--- 03_search_dau/solution.py
+++ 03_search_dau/solution.py
@@ -1,4 +1,7 @@
-from yt.wrapper import YtClient
+from yt.wrapper import YtClient, TablePath, TypedJob, yt_dataclass
+from datetime import datetime, date
+from typing import Iterable
+from yt.wrapper.schema import RowIterator
 
 
 SOURCE_PATH = "//home/student/logs/user_activity_log"
@@ -6,12 +9,77 @@ SOURCE_PATH = "//home/student/logs/user_activity_log"
 TARGET_PATH = "//home/student/assignments/search_dau/output"
 
 
+@yt_dataclass
+class Users:
+    userid: str
+    action: str
+    timestamp: datetime
+
+
+@yt_dataclass
+class UserDate:
+    userid: str
+    timestamp: date
+
+
+@yt_dataclass
+class DAU:
+    date: date
+    users: int
+
+
+class SimpleMap(TypedJob):
+    def __call__(self, data: Users) -> Iterable[UserDate]:
+        if data.action == "search":
+            yield UserDate(
+                userid=data.userid,
+                timestamp=data.timestamp.date()
+            )
+
+
+class SimpleReduce(TypedJob):
+    def __call__(self, data: RowIterator[UserDate]) -> Iterable[UserDate]:
+        for i in data:
+            yield i
+            break
+        
+
+class TrivialMapUsers(TypedJob):
+    def __call__(self, data: UserDate) -> Iterable[UserDate]:
+        yield data
+
+
+class ReduceToDAU(TypedJob):
+    def __call__(self, data: UserDate) -> Iterable[DAU]:
+        count = 0
+        dt = None
+        for i in data:
+            count += 1
+            dt = i.timestamp
+        yield DAU(
+            date=dt,
+            users=count
+        )
+
 
 def main():
     client = YtClient(proxy="127.0.0.1:8000", config={"proxy": {"enable_proxy_discovery": False}})
-    ...
+    
+    client.run_map_reduce(
+        SimpleMap(),
+        SimpleReduce(),
+        source_table=SOURCE_PATH,
+        destination_table="//tmp/Search_DAU_1",
+        reduce_by=["timestamp", "userid"]
+    )
 
-
+    client.run_map_reduce(
+        TrivialMapUsers(),
+        ReduceToDAU(),
+        source_table="//tmp/Search_DAU_1",
+        destination_table=TARGET_PATH,
+        reduce_by=["timestamp"]
+    )
 
 if __name__ == "__main__":
     main()
--- 04_revenue_by_month/solution.py
+++ 04_revenue_by_month/solution.py
@@ -1,16 +1,62 @@
 
-from yt.wrapper import YtClient
-
+from yt.wrapper import YtClient, yt_dataclass, TypedJob
+from yt.wrapper.schema import RowIterator
+from datetime import datetime, date
+from typing import Iterable  
 
 SOURCE_PATH = "//home/student/logs/user_activity_log"
 
 TARGET_PATH = "//home/student/assignments/revenue_by_month/output"
 
 
+@yt_dataclass
+class Users:
+    action: str
+    date: datetime
+    value: float
+
+
+@yt_dataclass
+class UsersDate:
+    month: str
+    value: float
+
+
+class SimpleMap(TypedJob):
+    def __call__(self, data: Users) -> Iterable[UsersDate]:
+        if data.action == "confirmation":
+            yield UsersDate(
+                month=data.date.strftime("%Y-%m"),
+                value=data.value
+            )
+
+
+class SimpleReduce(TypedJob):
+    def __call__(self, data: RowIterator[UsersDate]) -> Iterable[UsersDate]:
+        sum = 0
+        month = None
+        for i in data:
+            sum += i.value
+            month = i.date
+        
+        assert month is not None
+
+        yield UsersDate(
+            month=month,
+            value=sum
+        )
+
 
 def main():
     client = YtClient(proxy="127.0.0.1:8000", config={"proxy": {"enable_proxy_discovery": False}})
-    ...
+    
+    client.run_map_reduce(
+        SimpleMap(),
+        SimpleReduce(),
+        input_table=SOURCE_PATH,
+        destination_table=TARGET_PATH,
+        group_by=["month"]
+    )
 
 
 
