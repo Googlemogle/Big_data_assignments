745cdd76f7ff2489c29eeb1f44a1047efe836958
Googlemogle <89452865+Googlemogle@users.noreply.github.com>
2024-03-20 20:51:46 +0300

    Add files via upload

--- 10_no_checkouts_ratio/solution.py
+++ 10_no_checkouts_ratio/solution.py
@@ -1,10 +1,43 @@
-from yt.wrapper import YtClient
+from yt.wrapper import YtClient, yt_dataclass, TypedJob
+from yt.wrapper.schema import RowIterator
+from itertools import groupby
+from typing import Iterable
+from datetime import datetime, date
 
 
 SOURCE_PATH = "//home/student/logs/user_activity_log"
 
 TARGET_PATH = "//home/student/assignments/no_checkouts_ratio/output"
 
+
+@yt_dataclass
+class Day:
+    date: date
+    users_share: float
+
+
+@yt_dataclass
+class Users:
+    userid: str
+    timestamp: datetime
+    action: str
+
+
+class TrivialMap(TypedJob):
+    def __call__(self, data: Users) -> Iterable[Users]:
+        yield data
+
+
+class ReduceUsersToDate(TypedJob):
+    def __call__(self, data: RowIterator[Users]) -> Iterable[Day]:
+        uniqu = 0
+        target_users = 0
+
+        for i in groupby(data, key=lambda x: x.userid):
+            uniqu += 1
+            
+            for j in i[1]:
+                if j.action == ""
 
 
 def main():
